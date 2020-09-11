6544ea93ab11f247c445355ef127405a51a85304
justkhant <khant.kky@gmail.com>
2020-09-10 23:01:34 -0400

    Add intro banner and instructions.

--- Mangamop_Flask_App/mangaclean.py
+++ Mangamop_Flask_App/mangaclean.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return render_template('home.html')
+    return render_template('main.html')
 
 if __name__ == '__main__':
    app.run()
